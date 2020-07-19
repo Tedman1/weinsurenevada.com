76c2b6cc3ef411387c6e39d63be3f1d027a47582
Tedman1 <58830182+Tedman1@users.noreply.github.com>
2020-07-19 12:19:00 -0700

    Updates

---
+++ precache-manifest.92512c01087c9d35d02647375ceddfe7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c372ecde270f70d03cb5b9446ee39a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02adcd26fcf5031c8c18",
+    "url": "/static/css/2.c01c6cce.chunk.css"
+  },
+  {
+    "revision": "6c7e4c925cf7e7a4ee79",
+    "url": "/static/css/main.c04e7e51.chunk.css"
+  },
+  {
+    "revision": "02adcd26fcf5031c8c18",
+    "url": "/static/js/2.5c2bb8a4.chunk.js"
+  },
+  {
+    "revision": "0173e6ae5873aefd516e7516913022b6",
+    "url": "/static/js/2.5c2bb8a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c7e4c925cf7e7a4ee79",
+    "url": "/static/js/main.4d26c5e0.chunk.js"
+  },
+  {
+    "revision": "9aba0803504a29667ff2",
+    "url": "/static/js/runtime-main.65706881.js"
+  },
+  {
+    "revision": "c38c0b47550e8ef5db2c5e71092ad362",
+    "url": "/static/media/HKGrotesk-Bold.c38c0b47.woff2"
+  },
+  {
+    "revision": "58192ae9b701ff8512cb5a9d8047543f",
+    "url": "/static/media/we-quote-nevada-logo-pink.58192ae9.svg"
+  }
+]);
